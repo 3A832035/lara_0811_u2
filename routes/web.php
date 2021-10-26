893553925e100817d4fc1bcf54b653d8de6c0b47
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-10-27 03:36:38 +0800

    hello, tom

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('r1', function () {
-    return redirect('r2');
-});
-
-Route::get('r2', function () {
-    return view('welcome');
+Route::get('hello/{name}', function($name) {
+    return 'hello, '.$name;
 });
