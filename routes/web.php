bf350253b064a3c064e661f284ba2ad8aa8e1539
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-10-27 04:20:18 +0800

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('dashboard', function() {
-    return 'dashboard';
-});
-
-Route::group(['prefix'=>'admin'],function(){
-    Route::get('dashboard',function(){
-        return'admindashboard';
-    });
-});
+Route::get('home', [HomeController::class, 'index'])->name('home.index');
