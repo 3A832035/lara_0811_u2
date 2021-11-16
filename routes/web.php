dc193eaecff13bf7a5b7d54532fa53091e2a11e1
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-11-17 02:22:24 +0800

    訂正route

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,30 @@ use App\Http\Controllers\HomeController;
 |
 */
 
+Route::get('/', function () {
+    //return view('welcome');
+    return 'welcome';
+});
+
+Route::get('r2', function() {
+    return view('welcome');
+});
+Route::get('r1', function() {
+    return redirect('r2');
+});
+
+Route::get('hello/{name?}', function($name='Everybody') {
+    return 'Hello, '.$name;
+})->name('hello.index');
+
+Route::get('dashboard', function() {
+    return 'dashboard';
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('dashboard', function() {
+        return 'admin dashboard';
+    });
+});
+
 Route::get('home', [HomeController::class, 'index'])->name('home.index');
